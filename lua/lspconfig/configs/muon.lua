5212844ee859d624a3e45c6b07f29361afcc0912
Stone Tickle <lattis@mochiro.moe>
2026-02-17 15:27:50 -0500

    feat(muon): don't specify root_dir
    muon no longer requires a root_dir.  The same instance of the server can
    handle multiple different projects

--- lua/lspconfig/configs/muon.lua
+++ lua/lspconfig/configs/muon.lua
@@ -11,12 +11,6 @@ return {
   default_config = {
     cmd = { 'muon', 'analyze', 'lsp' },
     filetypes = { 'meson' },
-    root_dir = function(fname)
-      local res = async.run_command({ 'muon', 'analyze', 'root-for', fname })
-      if res[1] then
-        return vim.trim(res[1])
-      end
-    end,
   },
   docs = {
     description = [[
